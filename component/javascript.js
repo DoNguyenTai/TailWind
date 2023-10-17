9e546553446e298da4d837c06ceddc525c12a84c
taibeo <donguyentai05@gmail.com>
2023-10-17 15:19:31 +0700

    add component

--- component/javascript.js
+++ component/javascript.js
@@ -1,120 +1,58 @@
-var watchList = [
-    {
-      "Title": "Inception",
-      "Year": "2010",
-      "Rated": "PG-13",
-      "Released": "16 Jul 2010",
-      "Runtime": "148 min",
-      "Genre": "Action, Adventure, Crime",
-      "Director": "Christopher Nolan",
-      "Writer": "Christopher Nolan",
-      "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
-      "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
-      "Language": "English, Japanese, French",
-      "Country": "USA, UK",
-      "imdbRating": "8.8",
-      "imdbVotes": "1,446,708",
-      "imdbID": "tt1375666",
-      "Type": "movie",
-    },
-    {
-      "Title": "Interstellar",
-      "Year": "2014",
-      "Rated": "PG-13",
-      "Released": "07 Nov 2014",
-      "Runtime": "169 min",
-      "Genre": "Adventure, Drama, Sci-Fi",
-      "Director": "Christopher Nolan",
-      "Writer": "Jonathan Nolan, Christopher Nolan",
-      "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
-      "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-      "Language": "English",
-      "Country": "USA, UK",
-      "imdbRating": "8.6",
-      "imdbVotes": "910,366",
-      "imdbID": "tt0816692",
-      "Type": "movie",
-    },
-    {
-      "Title": "The Dark Knight",
-      "Year": "2008",
-      "Rated": "PG-13",
-      "Released": "18 Jul 2008",
-      "Runtime": "152 min",
-      "Genre": "Action, Adventure, Crime",
-      "Director": "Christopher Nolan",
-      "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
-      "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
-      "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
-      "Language": "English, Mandarin",
-      "Country": "USA, UK",
-      "imdbRating": "9.0",
-      "imdbVotes": "1,652,832",
-      "imdbID": "tt0468569",
-      "Type": "movie",
-    },
-    {
-      "Title": "Batman Begins",
-      "Year": "2005",
-      "Rated": "PG-13",
-      "Released": "15 Jun 2005",
-      "Runtime": "140 min",
-      "Genre": "Action, Adventure",
-      "Director": "Christopher Nolan",
-      "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
-      "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
-      "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
-      "Language": "English, Urdu, Mandarin",
-      "Country": "USA, UK",
-      "imdbRating": "8.3",
-      "imdbVotes": "972,584",
-      "imdbID": "tt0372784",
-      "Type": "movie",
-    },
-    {
-      "Title": "Avatar",
-      "Year": "2009",
-      "Rated": "PG-13",
-      "Released": "18 Dec 2009",
-      "Runtime": "162 min",
-      "Genre": "Action, Adventure, Fantasy",
-      "Director": "James Cameron",
-      "Writer": "James Cameron",
-      "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
-      "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
-      "Language": "English, Spanish",
-      "Country": "USA, UK",
-      "imdbRating": "7.9",
-      "imdbVotes": "876,575",
-      "imdbID": "tt0499549",
-      "Type": "movie",
-    }
-  ];
+function generateHTML(value) {
+    const html = `
+    <section>
+    <div class="container px-5 py-24 mx-auto flex flex-wrap ">
+        <div class="w-full mx-auto">
+            <div class="flex w-4/5 justify-center mx-auto ">
+                    <div class="w-1/5 flex items-center relative">
+                        <div class="h-1 w-full  absolute  ">
+                            <div class=" bg-red-500 h-1 w-full bg-gray-200 "></div>
+                            <h3 class="pt-6">Model</h3>
+                        </div>
+                        <div class="w-6 h-6 bg-white relative rounded-full border-[6px] border-red-500"></div>         
+                    </div>
+                    <div class="w-1/5 flex items-center relative">
+                        <div class="h-1 w-full  absolute  ">
+                            <div class="  h-1 w-full bg-gray-200 "></div>
+                            <h3 class="pt-6">Repair</h3>
+                        </div>
+                        <div class="w-6 h-6 bg-white relative rounded-full border-[6px] border-red-500"></div>
+                    </div>
+                    <div class="w-1/5 flex items-center relative">
+                        <div class="h-1 w-full  absolute  ">
+                            <div class="  h-1 w-full bg-gray-200 "></div>
+                            <h3 class="pt-6">Review</h3>
+                        </div>
+                        <div class="w-6 h-6 bg-white relative rounded-full border-[6px] border-red-500"></div>
+                    </div>
+                    <div class="w-1/5 flex items-center relative">
+                        <div class="h-1 w-full  absolute  ">
+                            <div class="  h-1 w-full bg-gray-200 "></div>
+                            <h3 class="pt-6">Pay</h3>
+                        </div>
+                        <div class="w-6 h-6 bg-white relative rounded-full border-[6px] border-red-500"></div>
+                    </div>
+                    <div class="w-1/5 flex items-center relative">
+                        <div class="h-1 w-full  absolute  ">
+                            <h3 class="pt-6">Complete</h3>
+                        </div>
+                        <div class="w-6 h-6 bg-white relative rounded-full border-[6px] border-red-500"></div>
+                    </div>   
+            </div>
+        </div>
+    </div>
+  </section>
+    `;
+    return html;
   
-    
 
+ 
+}
 
+// Sử dụng hàm để lấy đoạn mã HTML
+const myHTML = generateHTML(2);
 
+// In ra kết quả
 
-//   var filmAuthor = watchList.filter(function(course){
-//     return course.Director == 'Christopher Nolan'
-//   })
-//   var totalIMDB = watchList.reduce(function(value, score) {
-//         // return (Number.parseInt(value) + Number.parseInt(score.imdbRating))
-//         console.log( Number.parseFloat(score.imdbRating))
-//   }, 0)
-//   console.log(totalIMDB)
-  
-  
-  
-  
-  
-  // Expected results
-  console.log(calculateRating(watchList)); // Output: 8.675
-  
-  
-  
-  
-  
-  
-  
+let a = document.querySelector(".a")
+a.innerHTML = myHTML
